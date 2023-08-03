1d041dd8a66da4b05e1bd5cab9e76947245aeb84
Alexandru Scheusan <andu.scheusan@gmail.com>
2023-08-03 08:30:04 +0300

    Final code + Commented

--- src/com/fractals/BoxCounting.java
+++ src/com/fractals/BoxCounting.java
@@ -8,85 +8,123 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 
 public class BoxCounting {
+
+    // this method calculates the fractal dimension using linear regression
     public static double fractalDimension(boolean[][] matrix, double threshold) {
+
         int width = matrix.length;
         int height = matrix[0].length;
 
-        // Count boxes
+        // counting the boxed
         int[] boxes = new int[10];
+
         for (int i = 1; i < 10; i++) {
+
             int boxSize = width / i;
+
             for (int x = 0; x < i; x++) {
+
                 for (int y = 0; y < i; y++) {
+
                     boolean isBoxEmpty = true;
-                    for (int j = x * boxSize; j < (x + 1) * boxSize; j++) {
-                        for (int k = y * boxSize; k < (y + 1) * boxSize; k++) {
+
+                    for (int j = x * boxSize; j < (x + 1) * boxSize && j < width; j++) {
+
+                        for (int k = y * boxSize; k < (y + 1) * boxSize && k < height; k++) {
+
                             if (matrix[j][k]) {
+
                                 isBoxEmpty = false;
+
                                 break;
+
                             }
                         }
                         if (!isBoxEmpty) {
+
                             break;
+
                         }
                     }
                     if (!isBoxEmpty) {
+
                         boxes[i]++;
+
                     }
                 }
             }
         }
 
-        // Linear regression (logarithmic)
+        // linear regression
         double sumX = 0;
         double sumY = 0;
         double sumXY = 0;
         double sumXX = 0;
+
         for (int i = 1; i < 10; i++) {
+
             double logI = Math.log(1.0 / i);
             double logBoxes = Math.log(boxes[i]);
+
             sumX += logI;
             sumY += logBoxes;
             sumXY += logI * logBoxes;
             sumXX += logI * logI;
+
         }
 
         double n = 9.0;
         double slope = (n * sumXY - sumX * sumY) / (n * sumXX - sumX * sumX);
+
         return -slope;
     }
 
     public static boolean[][] loadImage(String filePath) throws IOException {
+
         BufferedImage image = ImageIO.read(new File(filePath));
+
         int width = image.getWidth();
         int height = image.getHeight();
+
         boolean[][] matrix = new boolean[width][height];
+
         for (int x = 0; x < width; x++) {
             for (int y = 0; y < height; y++) {
+
                 int rgb = image.getRGB(x, y);
+
                 Color color = new Color(rgb, true);
+
                 matrix[x][y] = color.getAlpha() > 0 && (color.getRed() + color.getGreen() + color.getBlue()) / 3 < 128;
+
             }
         }
+
         return matrix;
     }
 
     public static void main(String[] args) {
         try {
-            // Load image
+
+            // Loading the image
             String filePath = "/Users/AnduScheusan/Documents/Coding/Java/Box-Counting/src/com/fractals/pictures/fractal.png";
             boolean[][] matrix = loadImage(filePath);
 
-            // Calculate fractal dimension
+            // Calculating the fractal dimension
             double threshold = 0.9;
             double dimension = fractalDimension(matrix, threshold);
 
-            // Print the fractal dimension
+            // Printing the fractal dimension
             System.out.println("Fractal dimension: " + dimension);
+
         }catch (FileNotFoundException e){
+
             System.out.println("File not found!!!");
+
         } catch (IOException e) {
+
             e.printStackTrace();
+
         }
     }
 }
--- src/com/fractals/BoxCountingGUI.java
+++ src/com/fractals/BoxCountingGUI.java
@@ -17,49 +17,78 @@ public class BoxCountingGUI extends JFrame {
     private JLabel imageLabel;
 
     public BoxCountingGUI() {
+        
+        // Setting the application name
         setTitle("Fractal Dimension Calculator");
+
+        // Setting the mode of work for the application
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        
+        // Setting the layout type
         setLayout(new FlowLayout());
 
-        ImageIcon imageIcon = new ImageIcon("/Users/AnduScheusan/Documents/Coding/Java/Box-Counting/src/com/fractals/media/icon.png");
+        // Setting the icon
+        ImageIcon logo = new ImageIcon("./media/icon.jpg");
+        setIconImage(logo.getImage());
 
-        setIconImage(imageIcon.getImage());
-
+        // Setting the app title
         appTitle = new JLabel("Fractal Dimension Calculator");
 
+        // Creating the image selector button
         JButton selectImageButton = new JButton("Choose Image");
+
+        // Choosing the image and then returning the filepath
         selectImageButton.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
+
                 JFileChooser fileChooser = new JFileChooser();
+                
                 FileNameExtensionFilter filter = new FileNameExtensionFilter("Images", "jpg", "jpeg", "png");
+
                 fileChooser.setFileFilter(filter);
+
                 int returnValue = fileChooser.showOpenDialog(BoxCountingGUI.this);
+
                 if (returnValue == JFileChooser.APPROVE_OPTION) {
+
                     String imagePath = fileChooser.getSelectedFile().getAbsolutePath();
+
                     try {
+
                         BufferedImage image = ImageIO.read(new File(imagePath));
+
                         ImageIcon imageIcon = new ImageIcon(image);
+
                         imageLabel.setIcon(imageIcon);
 
                         boolean[][] matrix = BoxCounting.loadImage(imagePath);
                         double threshold = 0.9;
                         double dimension = BoxCounting.fractalDimension(matrix, threshold);
+
                         resultLabel.setText("Fractal dimension: " + dimension);
+
                     } catch (IOException ex) {
+
                         resultLabel.setText("Error: " + ex.getMessage());
+
                     }
                 }
             }
         });
 
+        // Aligning the file choose button in page
+        selectImageButton.setAlignmentX(40);
+        selectImageButton.setAlignmentY(300);
+
+        // Customizations
+        appTitle.setFont(new Font("Roboto", Font.BOLD, 30));
         appTitle.setPreferredSize(new Dimension(800, 60));
-        appTitle.setFont(new Font("Verdana", Font.BOLD, 30));
         appTitle.setHorizontalAlignment(SwingConstants.CENTER);
         appTitle.setVerticalAlignment(SwingConstants.TOP);
 
         resultLabel = new JLabel();
         resultLabel.setPreferredSize(new Dimension(300, 60));
-        resultLabel.setFont(new Font("Verdana", Font.PLAIN, 16));
+        resultLabel.setFont(new Font("Roboto", Font.PLAIN, 16));
 
         imageLabel = new JLabel();
         imageLabel.setPreferredSize(new Dimension(350, 350));
@@ -69,10 +98,16 @@ public class BoxCountingGUI extends JFrame {
         add(imageLabel);
         add(resultLabel);
 
+
+        // Final Touches
         pack();
+
         setSize(800, 500);
+
         setLocationRelativeTo(null);
+
         setVisible(true);
+
     }
 
     public static void main(String[] args) {
